05de4f2e37f846f16e53430c3ebec8b5a1dac352
Martin-N-Menendez <menendez91@live.com.ar>
2021-09-27 19:59:13 -0300

    RNA in progress

--- ATA.py
+++ ATA.py
@@ -1,4 +1,5 @@
-def ATA():
-    print("#"*20+" Starting Automatic Test Analyzer "+"#"*20)
-    print("Reading formal specifications")
-    print("Generating test specifications")
+def ATA(test = False):
+    if test:
+        print("#"*20+" Starting Automatic Test Analyzer "+"#"*20)
+        print("Reading formal specifications")
+        print("Generating test specifications")
